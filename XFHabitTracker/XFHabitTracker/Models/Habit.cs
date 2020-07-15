02ab51d4ea426466241de1223b01773240357aa2
shuheydev <shuhey.dev@gmail.com>
2020-07-15 15:41:03 +0900

    習慣を記録するモデルを作成.
    はじめはシンプルに.

---
+++ XFHabitTracker/XFHabitTracker/Models/Habit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFHabitTracker.Models
+{
+    public class Habit
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
+
+        public List<Record> Records { get; set; } = new List<Record>();
+    }
+
+    public class Record
+    {
+        public string Id { get; set; }
+        public DateTimeOffset Start { get; set; }
+        public DateTimeOffset End { get; set; }
+    }
+}
