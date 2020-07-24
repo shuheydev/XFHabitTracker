7be8e61f79b333edb9579c608f0dfdabb17ef4e7
shuheydev <shuhey.dev@gmail.com>
2020-07-24 13:37:44 +0900

    habitlistpageをC#マークアップ拡張で記述した.

--- XFHabitTracker/XFHabitTracker/Views/HabitListPage.xaml.cs
+++ XFHabitTracker/XFHabitTracker/Views/HabitListPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Syncfusion.ListView.XForms;
+using Syncfusion.XForms.Buttons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
 using Xamarin.Forms.Xaml;
 using XFHabitTracker.Models;
 using XFHabitTracker.Services;
@@ -12,19 +15,65 @@ using XFHabitTracker.ViewModels;
 
 namespace XFHabitTracker.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HabitListPage : ContentPage
     {
         private readonly HabitListViewModel _viewModel;
 
         public HabitListPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            Build();
 
-            var a = Startup.ServiceProvider.GetService<IDataStore<Habit>>();
-
-            this.BindingContext =_viewModel= Startup.ServiceProvider.GetService<HabitListViewModel>();
+            this.BindingContext = _viewModel = Startup.ServiceProvider.GetService<HabitListViewModel>();
         }
+
+        private void Build()
+        {
+            Content = new Grid
+            {
+                Children =
+                {
+                    HabitList,
+                    new SfButton{ Text="Add",
+                                  HorizontalOptions=LayoutOptions.End,
+                                  VerticalOptions=LayoutOptions.End,
+                    }.Margins(10,10,10,10)
+                    .Invoke(button=>{
+                        button.Clicked+=AddHabitButtonClicked;
+                    }),
+                    new Label{
+                        BackgroundColor=Color.Yellow,
+                        HorizontalOptions=LayoutOptions.Start,
+                        VerticalOptions=LayoutOptions.End
+                    }.FormattedText(new Span{}.Bind(Span.TextProperty,nameof(HabitListViewModel.ActionType)),
+                                    new Span{Text=" "},
+                                    new Span{}.Bind(Span.TextProperty,nameof(HabitListViewModel.HabitId)))
+                }
+            };
+        }
+
+        SfListView HabitList = new SfListView
+        {
+            AutoFitMode = AutoFitMode.Height,
+            DragStartMode = DragStartMode.OnHold,
+            FooterSize = 200,
+            ItemSpacing = 0.5,
+            DragItemTemplate = new DataTemplate(() => HabitListItemTemplate(Color.LightPink)),
+            ItemTemplate = new DataTemplate(() => HabitListItemTemplate(Color.LightBlue)),
+            FooterTemplate = new DataTemplate(() => new Grid { }.Height(100)),
+        }.Bind(SfListView.ItemsSourceProperty, nameof(HabitListViewModel.Habits));
+
+        private static StackLayout HabitListItemTemplate(Color bgColor) => new StackLayout
+        {
+            BackgroundColor = bgColor,
+            Children =
+                {
+                    new Label{}.Bind(Label.TextProperty,nameof(Habit.Name)),
+                    new Label{}.Bind(Label.TextProperty,nameof(Habit.Id)),
+                }
+        };
+
 
         protected override async void OnAppearing()
         {
