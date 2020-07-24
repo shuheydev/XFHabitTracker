87766608390356902664434ffef1178788b03fb1
shuheydev <shuhey.dev@gmail.com>
2020-07-24 13:39:08 +0900

    SfTextInputLayoutで例外が発生する.解決策がわからない.

--- XFHabitTracker/XFHabitTracker/Views/AddHabitPage.xaml.cs
+++ XFHabitTracker/XFHabitTracker/Views/AddHabitPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MvvmHelpers;
+using Syncfusion.XForms.Buttons;
+using Syncfusion.XForms.TextInputLayout;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
 using Xamarin.Forms.Xaml;
 using XFHabitTracker.ViewModels;
 
@@ -18,9 +21,26 @@ namespace XFHabitTracker.Views
 
         public AddHabitPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            Build();
 
             this.BindingContext = _viewModel = Startup.ServiceProvider.GetService<AddHabitViewModel>();
+        }
+
+        private void Build()
+        {
+            //SfTextInputLayouで例外が発生してしまう.どうしたらいいんだ.
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new SfTextInputLayout{
+                        Hint="Name",
+                        InputView=new Entry{Placeholder="habit name"}.Bind(Entry.TextProperty,nameof(AddHabitViewModel.Name)),
+                    },
+                    new SfButton{Text="Ok"}.BindCommand(nameof(AddHabitViewModel.AddNewHabitCommand))
+                }
+            };
         }
     }
 }
